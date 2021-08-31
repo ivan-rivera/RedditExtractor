96db31da795220100acaf2fb6e9a029f33e470fe
ivan.s.rivera@gmail.com <ivan.s.rivera@gmail.com>
2021-08-31 19:42:13 +0200

    added url encoder

--- R/url_to_json.R
+++ R/url_to_json.R
@@ -1,6 +1,7 @@
 # Read URL into a JSON format
 url_to_json <- function(url) url |>
   throttle() |>
+  utils::URLencode() |>
   readLines(warn = FALSE) |>
   RJSONIO::fromJSON() |>
   tryCatch(error = function(e) stop("Cannot read from Reddit, check your inputs or internet connection"))
